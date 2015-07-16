fa06336110e6db4c8cc6034f74042d872cadfd2d
Ryan <marco262@gmail.com>
2015-07-15 23:18:53 -0700

    Added caching
    Removed preloading in favor of caching whenever a resource is needed.
    Renamed "resources" to "resources_cache".

--- config_helper.py
+++ config_helper.py
@@ -3,8 +3,7 @@ import PIL_Helper as ph
 from ConfigParser import SafeConfigParser, NoOptionError
 
 config = None
-resources = None
-preload_assets = None
+resources_cache = None
 
 pjoin = os.path.join
 
@@ -23,7 +22,7 @@ def LoadConfig(folder, card_set, filename):
 
     Some config options are meant to have the assets preloaded.
     '''
-    global config, resources
+    global config, resources_cache
     # Init config object
     config = SafeConfigParser()
     # Load main config
@@ -33,39 +32,24 @@ def LoadConfig(folder, card_set, filename):
     if os.path.exists(alt_config_path):
         config.read(alt_config_path)
     # Parse all necessary config options into ready-made objects
-    # and store them in the resources object
+    # and store them in the resources_cache object
     print __file__
-    if get("preload assets", val_type="bool", default=True):
-        resources = {}
-        parse_fonts(folder)
+    resources_cache = {}
 
-def parse_fonts(folder):
-    '''
-    Parses every matching "fontfile_*" and "fontsize_*" pair in every config
-    section, and adds a correspending "font_*" object to that section, which
-    is the created font object.
-    '''
-    fonts_directory = get("fonts directory", "Card Defaults")
-    if fonts_directory is None:
-        raise FontParseError("No 'fonts_directory' found in config file.")
-    fonts_directory = pjoin(folder, fonts_directory)
-    # Go through every section in the config file, except for Defaults
-    for section in config.sections():
-        if section is not "Defaults":
-            for option, value in config.items(section):
-                # If we find a fontfile_* option, find its fontsize_* pair
-                if option.startswith("fontfile_"):
-                    print_resources()
-                    # Pull name of the font object from config option
-                    # Note: Not the font filename, but the name applied to it within
-                    # the code. E.g. "Flavortext"
-                    fontname = option[9:]
-                    # If the section (e.g. Pony) doesn't exist in resources, create it.
-                    if section not in resources:
-                        resources[section] = {}
-                    resources[section]["font_"+fontname] = loadfont(
-                        section, fontname, fonts_directory
-                        )
+def add_to_resources(section, option, value):
+    # If the section (e.g. Pony) doesn't exist in resources_cache, create it.
+    if section not in resources_cache:
+        resources_cache[section] = {}
+    resources_cache[section][option] = value
+
+def find_option_in_config(option, card_type=None, card_name=None):
+    if config.has_section(card_name) and config.has_option(card_name, option):
+        return card_name
+    if config.has_section(card_type) and config.has_option(card_type, option):
+        return card_type
+    if config.has_section("Card Defaults") and config.has_option("Card Defaults", option):
+        return "Card Defaults"
+    return None
 
 def get(option, section="Settings", val_type="str", default=None):
     '''
@@ -104,33 +88,49 @@ def get_resource(option, card_type=None, card_name=None):
     @param str card_name: If not None, will check this section for the given option
         before 'card_type'.
 
-    Attempts to return the value for the given option from the resources dict, first
-        from a section matching card_name, then from a section matching card_type,
-        then the Card Defaults section. If none of the sections (if they exist)
-        have that option, then None is returned.
+    Attempts to return the value for the given option from the resources_cache dict,
+        first from a section matching card_name, then from a section matching
+        card_type, then the Card Defaults section. If none of the sections
+        (if they exist) have that option, then None is returned.
     All resources should be made the correct type as they're loaded during LoadConfig,
         so no checking of types is performed in this function.
 
     @return str or None: None, or the value from the config
     '''
-    if card_name in resources and option in resources[card_name]:
+    if card_name in resources_cache and option in resources_cache[card_name]:
         section = card_name
-    elif card_type in resources and option in resources[card_type]:
+    elif card_type in resources_cache and option in resources_cache[card_type]:
         section = card_type
-    elif "Card Defaults" in resources and option in resources["Card Defaults"]:
+    elif "Card Defaults" in resources_cache and option in resources_cache["Card Defaults"]:
         section = "Card Defaults"
     else:
         return None
-    return resources[section][option]
+    return resources_cache[section][option]
 
 def getfont(option, card_type=None, card_name=None):
-    if not preload_assets:
-        return loadfont(card_type, option.lower(), get("fonts directory", "Card Defaults"))
-    font = get_resource("font_"+option.lower(), card_type, card_name)
+    '''
+    Checks for a font object in the resources directory. If it's not there,
+    checks the config file and attempts to load the font into the resources_cache
+    directory. 
+    '''
+    # Name of the option for the font object
+    option = "font_"+option.lower()
+    # Check resources_cache for font object
+    font = get_resource(option, card_type, card_name)
     if font is None:
-        raise FontParseError("Font not found: {}|{}|{}".format(
-            option, card_type, card_name
-            ))
+        # If not in resources_cache, check if the fontfile_ option exists in the
+        # config.
+        section = find_option_in_config("fontfile_"+option.lower(),
+                                        card_type, card_name)
+        # If not, throw an error.
+        if section is None:
+            raise FontParseError("Font not found: {}|{}|{}".format(
+                option, card_type, card_name
+                ))
+        # Attempt to load a new font object from the config
+        font = loadfont(section, option, get("fonts directory", "Card Defaults"))
+        # Add that new font object to the resources_cache object.
+        add_to_resources(section, option, font)
     return font
 
 def loadfont(section, fontname, fonts_directory):
@@ -173,11 +173,11 @@ def print_config():
         print config.items(section)
 
 def print_resources():
-    if resources is None:
-        print resources
+    if resources_cache is None:
+        print resources_cache
         return
-    for section in resources:
-        print section, resources[section]
+    for section in resources_cache:
+        print section, resources_cache[section]
 
 class ConfigParseError(Exception):
     pass
