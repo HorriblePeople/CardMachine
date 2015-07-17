a217ec19bb16935627833a477bf4b056b009b988
Ryan <marco262@gmail.com>
2015-07-16 18:55:34 -0700

    Added BaBOC files

--- BaBOC/CardGen.py
+++ BaBOC/CardGen.py
@@ -19,11 +19,13 @@ VassalWorkspacePath = DIRECTORY+"/vassal workspace"
 VassalImagesPath = os.path.join(VassalWorkspacePath, "images")
 VassalCard = [0]
 
-bleed_w = 788
-bleed_h = 1088
+width = 788
+height = 1088
+width_center = width/2
+height_center = height/2
 w_marg = 31
 h_marg = 36
-bleedrect=[(w_marg,h_marg),(bleed_w-w_marg,bleed_h-h_marg)]
+bleedrect=[(w_marg,h_marg),(width-w_marg,height-h_marg)]
 textmaxwidth = 580
 LineM=PIL_Helper.Image.open(ResourcePath+"line_M.png")
 LineH=PIL_Helper.Image.open(ResourcePath+"line_H.png")
@@ -41,36 +43,46 @@ TypeFont = PIL_Helper.BuildFont(titleboldfont, 70)
 GenreFont = PIL_Helper.BuildFont(titleboldfont,50)
 FlavorFont = PIL_Helper.BuildFont("BaBOC/resources/KlinicSlabBookIt.otf", 40)
 CopyFont = PIL_Helper.BuildFont("BaBOC/resources/Barth_Regular.ttf", 10)
-TypeAnchor = (bleed_w/2+70, 50)
-TitleAnchor = (80, 60)
-FormTitleAnchor = (80, -60)
+TypeAnchor = (50, 520)
+TitleAnchor = (120, 40)
+FormTitleAnchor = (40, -60)
 SymbolAnchor = (80, -100)
-RulesAnchor = (bleed_w/2+70, 650)
-OneLineAnchor = (bleed_w/2+70, 160)
-TwoLineAnchor = (bleed_w/2+70, 220)
-FlavorAnchor = (bleed_w/2+70, -30)
+RulesAnchor = (width_center+70, 650)
+OneLineAnchor = (width_center+70, 160)
+TwoLineAnchor = (width_center+70, 220)
+FlavorAnchor = (width_center+70, -30)
+
 ColDict={
     "G": (225,200,225),
     "S": (225,255,225),
     "H": (255,225,225),
     "M": (225,225,255),
+    "A": (225,225,225),
+    "": (225,225,225),
     "+": (225,225,225),
     "-": (225,225,225)
     }
+
 ColDictDark={
     "G": (100,0,100),
     "S": (25,150,25),
     "H": (255,25,25),
     "M": (25,25,255),
+    "A": (225,225,225),
+    "": (225,225,225),
     "+": (225,225,225),
     "-": (125,125,125)
     }
+
 GenreDict={
     "G": "Grimdark",
     "S": "Sci-Fi",
     "H": "Hardcore",
+    "A": "All",
+    "": "All",
     "M": "Magick"
     }
+
 RulesDict={
     "FORM": "Counts as a Feature.\n+1 for every card matching your genre.",
     "FEATURE": "Play this card to your play area. You may attach Modifiers to this card.",
@@ -80,9 +92,7 @@ RulesDict={
     "GENRE CHANGE": "Change the genre of any card in your play area (even your Form). Can be used as an Interrupt."
     }
 
-
-
-def BuildCard( linein):
+def BuildCard(linein,filename=None):
     tags = linein.strip('\n').replace(r'\n', '\n').split('`')
     try:
         im = PickCardFunc(tags[TYPE], tags)
@@ -92,7 +102,13 @@ def BuildCard( linein):
         print "Warning, Bad Card: {0}".format(tags)
         traceback.print_exc()
     return im
-def PickCardFunc( card_type, tags):
+
+def BuildBack(linein):
+    tags = linein.strip('\n').replace(r'\n', '\n').split('`')
+    image = PIL_Helper.BlankImage(width, height)
+    return image
+
+def PickCardFunc(card_type, tags):
     if tags[TYPE] == "FORM":
         return MakeFormCard(tags)
     elif tags[TYPE] == "FEATURE":
@@ -103,15 +119,19 @@ def PickCardFunc( card_type, tags):
         return MakeFormModifierCard(tags)
     elif tags[TYPE] == "SWITCH":
         return MakeSwitchCard(tags)
+    elif tags[TYPE] == "FANDOM":
+        return MakeFandomCard(tags)
     elif tags[TYPE] == "GENRE CHANGE":
         return MakeGenreChangeCard(tags)
     elif tags[TYPE] == "BLANK":
         return MakeBlankCard()
     else:
-        raise Exception("No card of type {0}".format(tags[TYPE]))
-def DrawSidebar( image, color):
+        raise Exception("No card of type '{0}'".format(tags[TYPE]))
+
+def DrawSidebar(image, color):
     PIL_Helper.DrawRect(image, 0, 0, 160, 1088, color)
-def DrawLines(image,genres):
+
+def DrawLines(image, genres):
     for c in genres:
         if c=="G":
             image.paste(LineG,(0,660),LineG)
@@ -121,14 +141,17 @@ def DrawLines(image,genres):
             image.paste(LineM,(0,800),LineM)
         if c=="H":
             image.paste(LineH,(0,880),LineH)
-def TypeText( image, text):
+
+def TypeText(image, text):
     PIL_Helper.AddText(
         image = image,
         text = text,
         font = TypeFont,
-        anchor = TypeAnchor
+        anchor = TypeAnchor,
+        rotate = 270
         )
-def GenreText( image, text, color):
+
+def GenreText(image, text, color):
     PIL_Helper.AddText(
         image = image,
         text = text,
@@ -138,7 +161,8 @@ def GenreText( image, text, color):
         valign = "top",
         halign = "center",
         )
-def TitleText( image, text, color=(0, 0, 0)):
+
+def TitleText(image, text, color=(0, 0, 0)):
     print text
     PIL_Helper.AddText(
         image = image,
@@ -146,19 +170,20 @@ def TitleText( image, text, color=(0, 0, 0)):
         font = TitleFont,
         fill = color,
         anchor = TitleAnchor,
-        max_width = bleed_h-150,
+        max_width = height-150,
         leading_offset = 0,
-        rotate = 90
+        rotate = 270
         )
 
-def ValueText( image, text):
+def ValueText(image, text):
     PIL_Helper.AddText(
         image = image,
         text = text,
         font = ValueFont,
         anchor = (70,920)
         )
-def SymbolText( image, text):
+
+def SymbolText(image, text):
     font = BigSymbolFont if text == "-" else SymbolFont
     PIL_Helper.AddText(
         image = image,
@@ -167,7 +192,9 @@ def SymbolText( image, text):
         anchor = SymbolAnchor,
         valign = "center"
         )
-def RulesText( image, text):
+
+def RulesText(image, text):
+    '''Adds rules  text to the card'''
     PIL_Helper.AddText(
         image = image,
         text = text,
@@ -176,7 +203,9 @@ def RulesText( image, text):
         max_width = textmaxwidth,
         leading_offset = 0
         )
-def FlavorText( image, text):
+    
+def FlavorText(image, text):
+    '''Adds flavor text to the card'''
     PIL_Helper.AddText(
         image = image,
         text = text,
@@ -185,8 +214,9 @@ def FlavorText( image, text):
         max_width = textmaxwidth,
         valign = "bottom"
         )
+
 def MakeBlankCard():
-    image = PIL_Helper.BlankImage(bleed_w, bleed_h)
+    image = PIL_Helper.BlankImage(width, height)
     print("Blank Card")
     PIL_Helper.AddText(
         image = image,
@@ -197,39 +227,31 @@ def MakeBlankCard():
         max_width = textmaxwidth
         )    
     return image
+
 def MakeFormCard( tags):
-    image = PIL_Helper.BlankImage(bleed_w, bleed_h,
-                                  color=ColDict[tags[COLOR][0]]
-                                  )
+    image = PIL_Helper.BlankImage(width, height)
     TypeText(image, "Form")
-    GenreText(image,
-                   GenreDict[tags[COLOR][0]],
-                   ColDictDark[tags[COLOR][0]]
-                   )
-    # Form sidebar type
-    TitleText(image, "Form")
-    
-    # Form Title
-    PIL_Helper.AddText(
-        image = image,
-        text = tags[TITLE],
-        font = TitleFont,
-        fill = (0,0,0),
-        anchor = FormTitleAnchor,
-        max_width = bleed_h,
-        valign = "bottom",
-        rotate = 90
-        )
-    
-    RulesText(image, RulesDict["FORM"])
+    TitleText(image, tags[TITLE])
+    #DrawLines(image, ('G','S','M','H'))
+    # Add flavor text if it's in the list
     if len(tags) > FLAVOR:
         FlavorText(image, tags[FLAVOR])
-    #DrawLines(image,tags[COLOR])
     return image
+
+def MakeFandomCard( tags):
+    image = PIL_Helper.BlankImage(width, height)
+    TypeText(image, "Fandom")
+    RulesText(image, tags[TITLE])
+    #DrawLines(image, ('G','S','M','H'))
+    # Add flavor text if it's in the list
+    if len(tags) > FLAVOR:
+        FlavorText(image, tags[FLAVOR])
+    return image
+
 def MakeFeatureCard( tags):
-    image = PIL_Helper.BlankImage(bleed_w, bleed_h)
+    image = PIL_Helper.BlankImage(width, height)
     DrawSidebar(image, ColDict[tags[COLOR][0]])
-    DrawLines(image,tags[COLOR])
+    #DrawLines(image,tags[COLOR])
     TypeText(image, "Feature")
     GenreText(image,
                    GenreDict[tags[COLOR][0]],
@@ -241,10 +263,11 @@ def MakeFeatureCard( tags):
     if len(tags) > FLAVOR:
         FlavorText(image, tags[FLAVOR])
     return image
+
 def MakeModifierCard( tags):
-    image = PIL_Helper.BlankImage(bleed_w, bleed_h)
+    image = PIL_Helper.BlankImage(width, height)
     DrawSidebar(image, ColDict[tags[COLOR][0]])
-    DrawLines(image,tags[COLOR])
+    #DrawLines(image,tags[COLOR])
     TypeText(image, "Modifier")
     GenreText(image,
                    GenreDict[tags[COLOR][0]],
@@ -255,12 +278,12 @@ def MakeModifierCard( tags):
     RulesText(image, RulesDict["MODIFIER"])
     if len(tags) > FLAVOR:
         FlavorText(image, tags[FLAVOR])
-        
     return image
+
 def MakeFormModifierCard( tags):
-    image = PIL_Helper.BlankImage(bleed_w, bleed_h)
+    image = PIL_Helper.BlankImage(width, height)
     DrawSidebar(image, ColDict[tags[COLOR][0]])
-    DrawLines(image,tags[COLOR])
+    #DrawLines(image,tags[COLOR])
     TypeText(image, "Form Modifier")
     GenreText(image,
                    GenreDict[tags[COLOR][0]],
@@ -272,28 +295,31 @@ def MakeFormModifierCard( tags):
     if len(tags) > FLAVOR:
         FlavorText(image, tags[FLAVOR])
     return image
+
 def MakeSwitchCard( tags):
-    image = PIL_Helper.BlankImage(bleed_w, bleed_h)
-    DrawSidebar(image, ColDict[tags[COLOR][0]])
-    DrawLines(image,tags[COLOR])
+    image = PIL_Helper.BlankImage(width, height)
+    #DrawSidebar(image, ColDict[tags[COLOR][0]])
+    #DrawLines(image,tags[COLOR])
     TypeText(image, "Switch")
-    TitleText(image, tags[TITLE])
-    SymbolText(image, tags[COLOR][0])
-    RulesText(image, RulesDict["SWITCH"].format(tags[COLOR][0]))
+    #PMText = tags[TITLE].split("\\")
+    RulesText(image, tags[TITLE])
+    #SymbolText(image, tags[COLOR][0])
+    #RulesText(image, RulesDict["SWITCH"].format(tags[COLOR][0]))
     if len(tags) > FLAVOR:
         FlavorText(image, tags[FLAVOR])
     return image
+
 def MakeGenreChangeCard( tags):
-    image = PIL_Helper.BlankImage(bleed_w, bleed_h)
-    DrawLines(image,tags[COLOR])
+    image = PIL_Helper.BlankImage(width, height)
+    #DrawLines(image,tags[COLOR])
     TypeText(image, "Genre Change")
-    GenreText(image,
-                   GenreDict[tags[COLOR][0]],
-                   ColDictDark[tags[COLOR][0]]
-                   )
-    TitleText(image, tags[TITLE],
-                   color=ColDictDark[tags[COLOR][0]]
-                   )
+    #GenreText(image,
+    #               GenreDict[tags[COLOR][0]],
+    #               ColDictDark[tags[COLOR][0]]
+    #               )
+    #TitleText(image, tags[TITLE],
+    #               color=ColDictDark[tags[COLOR][0]]
+    #               )
     SymbolText(image, "<")
     RulesText(image, RulesDict["GENRE CHANGE"])
     if len(tags) > FLAVOR:
@@ -304,10 +330,12 @@ def BuildPage(card_list, page_num, page_width=PAGE_WIDTH, page_height=PAGE_HEIGH
     PIL_Helper.BuildPage(card_list, page_width, page_height,r"page_{0:>03}.png".format(page_num))
 
 def InitVassalModule(): pass
+
 def MakeVassalCard(im):
     VassalCard[0]+=1
     #BuildCard(line).save(VassalImagesPath + "/" + str(VassalCard) + ".png")
     im.save(VassalImagesPath + "\\" + str(VassalCard[0]) + ".png")
+
 def CompileVassalModule(): pass
 
 if __name__ == "__main__":
